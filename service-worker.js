5f07ec881fc26fa68f3d62f196f2e5d77998a955
k0serg <k0serg@internet.ru>
2025-07-02 16:57:34 +0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'vacation-calculator-cache-v7';
+const CACHE_NAME = 'vacation-calculator-cache-v8';
 const ASSETS_TO_CACHE = [
   '/vacation-calculator/',
   '/vacation-calculator/index.html',
